e65e07d4f946b6abf911872db0d2b7e940b035a9
frsyuki <frsyuki@users.sourceforge.jp>
2009-07-26 18:52:38 +0900

    update phraser

--- src/generator/phraser.rb
+++ src/generator/phraser.rb
@@ -1,25 +1,50 @@
 #
 # Phraser: Simple lexer and parser combinator
 #
-# Copyright (C) 2008-2009 FURUHASHI Sadayuki
+# Copyright (c) 2008-2009 FURUHASHI Sadayuki
 #
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#        http://www.apache.org/licenses/LICENSE-2.0
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 
 module Phraser
 
 
+class ScanError  < StandardError
+	def initialize(args)
+		@src = args[0]
+		@pos = args[1]
+		super(args[2])
+	end
+	attr_reader :src, :pos
+end
+
+class ParseError < StandardError
+	def initialize(args)
+		@tokens = args[0]
+		super(args[1])
+	end
+	attr_reader :tokens
+end
+
+
 class Scanner
+
 	class Token
 		def initialize(name, expr)
 			@name = name
@@ -47,15 +72,17 @@ class Scanner
 		until s.empty?
 			err = @tokens.each {|t|
 				if m = s.scan(t.expr)
+					pos = s.pos - m.length
 					(class<<m; self; end).instance_eval {
 						define_method(:token) { t.name }
 						define_method(:inspect) { "\"#{m}\":#{t.name}" }
+						define_method(:pos) { pos }
 					}
 					result.push(m)
 					break nil
 				end
 			}
-			raise "error '#{s.peek(10)}'" if err
+			raise ScanError, [src, s.pos, "error '#{s.peek(10)}'"] if err
 		end
 	
 		result
@@ -64,8 +91,6 @@ end
 
 
 class Parser
-	class ParseError < StandardError; end
-
 	def initialize(&block)
 		@block = block
 		@action = Proc.new {|x,e| x }
@@ -144,7 +169,7 @@ class Parser
 				r = parse(i,e)
 			rescue ParseError
 			end
-			raise ParseError, "not error: #{i.inpsect}" if r
+			raise ParseError, [i, "not error: #{i.inpsect}"] if r
 			[nil, i]
 		}
 	end
@@ -154,10 +179,15 @@ class Parser
 	end
 
 	class Context
-		def token(t)
+		def initialize(tmpl = nil)
+			@tmpl = tmpl
+		end
+		attr_reader :tmpl
+
+		def token(t, match = nil)
 			parser {|i,e|
-				unless i.first && i.first.token == t.to_sym
-					raise ParseError, "token error: #{i.inspect}"
+				unless i.first && i.first.token == t.to_sym && (match.nil? || i.first =~ match)
+					raise ParseError, [i, "token error: #{i.inspect}"]
 				end
 				[i.first, i[1..-1]]
 			}
@@ -166,7 +196,7 @@ class Parser
 		def any
 			parser {|i,e|
 				unless i.length > 0
-					raise ParseError, "any error: #{i.inspect}"
+					raise ParseError, [i, "any error: #{i.inspect}"]
 				end
 				[i.first, i[1..-1]]
 			}
@@ -175,7 +205,7 @@ class Parser
 		def eof
 			parser {|i,e|
 				unless i.empty?
-					raise ParseError, "EOF error: #{i.inspect}"
+					raise ParseError, [i, "EOF error: #{i.inspect}"]
 				end
 				[nil, i]
 			}
@@ -191,10 +221,20 @@ class Parser
 	end
 end
 
-def rule(&block)
-	Parser.new {|i,e|
-		Parser::Context.new.instance_eval(&block).parse(i)
-	}
+def rule(*arg_names, &block)
+	if arg_names.empty?
+		Parser.new {|i,e|
+			Parser::Context.new.instance_eval(&block).parse(i)
+		}
+	else
+		Proc.new {|*arg_values|
+			tmpl = {}
+			arg_names.zip(arg_values) {|k,v| tmpl[k] = v }
+			Parser.new {|i,e|
+				Parser::Context.new(tmpl).instance_eval(&block).parse(i)
+			}
+		}
+	end
 end
 
 
